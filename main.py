ae5c4502a828df15d7e332ef1c52700d20ae8d7c
Jose Quiceno <josequiceno000@gmail.com>
2025-12-31 14:56:33 -0600

    feat(main): print book contents to terminal

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+
+
+def get_book_text(filepath: str):
+    """Reads a book file and returns the contents as a string."""
+
+    with open(filepath, 'r') as file:
+        contents = file.read()
+    return contents
+
+
+
+
+def main():
+    book_contents = get_book_test("books/frankenstein.txt")
+    print(book_contents)
+
+if __name__ == "__main__":
+    main()
