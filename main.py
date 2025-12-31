626aab9632706d656bf4d14eb2fc1f237df5d3a4
Jose Quiceno <josequiceno000@gmail.com>
2025-12-31 14:59:28 -0600

    feat(main): return total words in book

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
+def count_words(text: str) -> int:
+    """Accepts text and returns the total number of words."""
+    return len(text.split())
 
 
-def get_book_text(filepath: str):
+def get_book_text(filepath: str) -> str:
     """Reads a book file and returns the contents as a string."""
 
     with open(filepath, 'r') as file:
         contents = file.read()
     return contents
 
-
-
-
 def main():
-    book_contents = get_book_test("books/frankenstein.txt")
-    print(book_contents)
+    book_contents = get_book_text("books/frankenstein.txt")
+    num_words = count_words(book_contents)
+    print(f"Found {num_words} total words")
 
 if __name__ == "__main__":
     main()
